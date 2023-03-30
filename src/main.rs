a1959f7d29c39702428c0902f3de8b3bf927e1fa
geekylthyosaur <geekylthyosaur@gmail.com>
2023-03-30 13:17:27 +0300

    Hello, world!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(index));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn index() -> &'static str {
+    "Hello, World!"
 }
