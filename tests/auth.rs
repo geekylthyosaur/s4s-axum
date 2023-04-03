b2608646bb2a2b90a5925adeaef4fddd21c0a71c
geekylthyosaur <geekylthyosaur@gmail.com>
2023-04-03 18:13:22 +0300

    Implement testing using fake data.

--- tests/auth.rs
+++ tests/auth.rs
@@ -1,61 +1,46 @@
 mod common;
 
-use std::error::Error;
-
 use axum::{
     body::Body,
     http::{header::CONTENT_TYPE, Method, Request, StatusCode},
 };
-use jsonschema::is_valid;
-use serde_json::{json, Value};
+use common::TestResult;
+use serde_json::Value;
 
 use crate::common::{DbPool, TestApp};
 
 #[sqlx::test]
-fn signup(pool: DbPool) -> Result<(), Box<dyn Error>> {
+fn signup(pool: DbPool) -> TestResult<()> {
     let mut app = TestApp::spawn(pool);
-    let signup_form = json!({
-        "username": "geekylthyosaur",
-        "email": "geekylthyosaur@gmail.com",
-        "password": "pwd",
-        "repeat_password": "pwd",
-    });
     let request = Request::builder()
         .method(Method::POST)
         .uri("/auth/signup")
         .header(CONTENT_TYPE, "application/json")
-        .body(Body::from(serde_json::to_vec(&signup_form)?))?;
+        .body(Body::from(serde_json::to_vec(
+            &TestApp::fake_signup_form_json(),
+        )?))?;
 
     let response = app.oneshot(request).await?;
 
     assert_eq!(response.status(), StatusCode::OK);
 
-    let schema = json!({
-        "type": "object",
-        "properties": {
-            "access_token": { "type": "string" },
-            "token_type": { "type": "string" }
-        },
-        "required": ["access_token", "token_type"]
-    });
+    let schema = TestApp::access_token_json_schema();
 
     let body = hyper::body::to_bytes(response.into_body()).await?;
     let body: Value = serde_json::from_slice(&body)?;
 
-    assert!(is_valid(&schema, &body));
+    assert!(schema.validate(&body).is_ok());
 
     Ok(())
 }
 
 #[sqlx::test]
-fn login(pool: DbPool) -> Result<(), Box<dyn Error>> {
+fn login(pool: DbPool) -> TestResult<()> {
     let mut app = TestApp::spawn(pool);
-    let signup_form = json!({
-        "username": "geekylthyosaur",
-        "email": "geekylthyosaur@gmail.com",
-        "password": "pwd",
-        "repeat_password": "pwd",
-    });
+
+    let signup_form = TestApp::fake_signup_form_json();
+    let login_form = TestApp::fake_login_form_json(&signup_form);
+
     let request = Request::builder()
         .method(Method::POST)
         .uri("/auth/signup")
@@ -63,11 +48,6 @@ fn login(pool: DbPool) -> Result<(), Box<dyn Error>> {
         .body(Body::from(serde_json::to_vec(&signup_form)?))?;
 
     let _ = app.oneshot(request).await?;
-
-    let login_form = json!({
-        "username": "geekylthyosaur",
-        "password": "pwd",
-    });
 
     let request = Request::builder()
         .method(Method::POST)
@@ -79,19 +59,12 @@ fn login(pool: DbPool) -> Result<(), Box<dyn Error>> {
 
     assert_eq!(response.status(), StatusCode::OK);
 
-    let schema = json!({
-        "type": "object",
-        "properties": {
-            "access_token": { "type": "string" },
-            "token_type": { "type": "string" }
-        },
-        "required": ["access_token", "token_type"]
-    });
+    let schema = TestApp::access_token_json_schema();
 
     let body = hyper::body::to_bytes(response.into_body()).await?;
     let body: Value = serde_json::from_slice(&body)?;
 
-    assert!(is_valid(&schema, &body));
+    assert!(schema.validate(&body).is_ok());
 
     Ok(())
 }
