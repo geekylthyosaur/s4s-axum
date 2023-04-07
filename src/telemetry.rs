0f539c18d1af5c71810e49b25823de06c884e30f
geekylthyosaur <geekylthyosaur@gmail.com>
2023-04-07 16:13:12 +0300

    Add EnvFilter for telemetry.

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,12 +1,13 @@
 use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, Registry};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Registry};
 
 pub struct Telemetry;
 
 impl Telemetry {
     pub fn initialize() {
         let stdout = tracing_subscriber::fmt::layer().pretty();
-        Registry::default().with(stdout).init();
+        let filter = EnvFilter::try_from_default_env().unwrap();
+        Registry::default().with(stdout).with(filter).init();
         LogTracer::init().expect("Failed to set logger!");
     }
 }
